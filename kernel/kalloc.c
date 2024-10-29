5456cb9be2aa499f7859369275713d5a254ff851
Simon <1907853454@qq.com>
2024-10-29 11:15:21 +0000

    kalloc.ok

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,23 +18,29 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem{
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
 
+struct kmem kmems[NCPU];
+//为每个cpu分配一个空间
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i=0;i<NCPU;i++){
+    initlock(&kmems[i].lock, "kmem");
+    printf("%d\n",i);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
+//这里的end是空闲空间起始地址
 void
 freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
+  p = (char*)PGROUNDUP((uint64)pa_start) + PGSIZE;
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
 }
@@ -46,6 +52,9 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  push_off();
+  int cpu_id = cpuid();
+  // printf("%d\n",cpu_id);
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -55,11 +64,12 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+//同时放入了freelist中
+  acquire(&kmems[cpu_id].lock);
+  r->next = kmems[cpu_id].freelist;
+  kmems[cpu_id].freelist = r;
+  release(&kmems[cpu_id].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +78,32 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+  push_off();
+  int cpu_id = cpuid();
+
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  acquire(&kmems[cpu_id].lock);
+  r = kmems[cpu_id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
-
+    kmems[cpu_id].freelist = r->next;
+  release(&kmems[cpu_id].lock);
+  int temp = cpu_id;
+  while(!r){
+    cpu_id++;
+    cpu_id = cpu_id % NCPU;
+    if(cpu_id==temp){
+      pop_off();
+      return 0;
+    }
+    acquire(&kmems[cpu_id].lock);
+    r = kmems[cpu_id].freelist;
+    if(r)
+    kmems[cpu_id].freelist = r->next;
+    release(&kmems[cpu_id].lock);
+  }
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  pop_off();
   return (void*)r;
 }
